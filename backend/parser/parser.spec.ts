ddd790751d34e2696f99cfd647b108ac95896869
Michael <michael.spengler@sap.com>
2022-10-17 13:07:16 +0200

    improve parser

--- backend/parser/parser.spec.ts
+++ backend/parser/parser.spec.ts
@@ -3,6 +3,43 @@ import { Parser } from "./parser.ts"
 
 const classUnderTest = Parser.getInstance()
 
+Deno.test("test addChildTo", async () => {
+    const root = { "host": "Michael", "invitees": [{ "host": "Marvena", "invitees": [{ "host": "Enyo", "invitees": [] }, { "host": "Becky", "invitees": [] }] }, { "host": "Kateryna", "invitees": [] }] }
+
+    let childByName = classUnderTest.getChildByName("Enyo", root)
+    assertEquals(childByName, { host: 'Enyo', invitees: [] })
+    childByName = classUnderTest.getChildByName("Kateryna", root)
+    assertEquals(childByName, { host: 'Kateryna', invitees: [] })
+    childByName = classUnderTest.getChildByName("Becky", root)
+    assertEquals(childByName, { host: 'Becky', invitees: [] })
+    childByName = classUnderTest.getChildByName("Marvena", root)
+    assertEquals(childByName, { host: "Marvena", invitees: [{ host: 'Enyo', invitees: [] }, { host: 'Becky', invitees: [] }] })
+
+})
+
+Deno.test("test addChildTo", async () => {
+    const root = {
+        host: 'Michael',
+        invitees: [
+            { host: 'Marvena', invitees: [] },
+            { host: 'Kateryna', invitees: [] }
+        ]
+    }
+
+    let child = { host: 'Enyo', invitees: [] }
+    let enrichedRoot = classUnderTest.addChildTo("Marvena", root, child)
+    child = { host: 'Becky', invitees: [] }
+    enrichedRoot = classUnderTest.addChildTo("Marvena", enrichedRoot, child)
+    let childByName = classUnderTest.getChildByName("Becky", enrichedRoot)
+    assertEquals(childByName, { host: 'Becky', invitees: [] })
+    child = { host: 'Puppy Fan', invitees: [] }
+    enrichedRoot = classUnderTest.addChildTo("Becky", enrichedRoot, child)
+    console.log(JSON.stringify(enrichedRoot))
+    childByName = classUnderTest.getChildByName("Puppy Fan", enrichedRoot)
+    assertEquals(childByName, { host: 'Puppy Fan', invitees: [] })
+
+})
+
 
 Deno.test("test addChild", async () => {
     const root = {
@@ -15,7 +52,7 @@ Deno.test("test addChild", async () => {
     let child = { host: 'Marvena', invitees: [{ host: 'Enyo', invitees: [] }, { host: 'Becky', invitees: [] }] }
     let enrichedRoot = classUnderTest.addChild(root, child)
     let children = classUnderTest.getChildren(enrichedRoot)
-    
+
     assertEquals(children, [{ host: "Kateryna", invitees: [] }, { host: "Marvena", invitees: [{ host: 'Enyo', invitees: [] }, { host: 'Becky', invitees: [] }] }])
     let childByName = classUnderTest.getChildByName("Marvena", enrichedRoot)
     assertEquals(childByName, { host: "Marvena", invitees: [{ host: 'Enyo', invitees: [] }, { host: 'Becky', invitees: [] }] })
--- backend/parser/parser.ts
+++ backend/parser/parser.ts
@@ -1,4 +1,4 @@
-import {IInviteInfo} from "../data-model.ts"
+import { IInviteInfo } from "../data-model.ts"
 export class Parser {
 
     private static instance: Parser
@@ -18,21 +18,37 @@ export class Parser {
     }
 
     public getChildByName(name: string, input: IInviteInfo): IInviteInfo {
+        let index = 0
         let childByName = input.invitees.filter((entry: IInviteInfo) => entry.host === name)[0]
+
         if (childByName === undefined) {
-            let index = 0
+            while (input.invitees.length > 0) {
+                childByName = this.getChildByName(name, input.invitees[index])
+                if (childByName !== undefined) {
+                    return childByName
+                }
+                index++
+            }
+            index = 0
             while (input.invitees[index]) {
                 childByName = this.getChildByName(name, input.invitees[index])
                 index++
             }
+            index++
         }
         return childByName
     }
 
     public addChildTo(hostName: string, host: IInviteInfo, child: IInviteInfo): IInviteInfo {
-        // host.invitees.push(child)
+        const pregnantHost = this.getChildByName(hostName, host)
+        if (pregnantHost === undefined) {
+            console.log(`hmm: ${hostName} not found in ${JSON.stringify(host)}`)
+        }
+        this.addChild(pregnantHost, child)
+
         return host
     }
+
     public addChild(host: IInviteInfo, child: IInviteInfo): IInviteInfo {
         host.invitees.push(child)
         return host
