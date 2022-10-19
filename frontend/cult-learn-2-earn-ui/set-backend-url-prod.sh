14f5e7e6a70bd85871b4ff6c7b79bf693a5dc4eb
Michael <michael.spengler@sap.com>
2022-10-19 23:11:58 +0200

    ops

---
+++ frontend/cult-learn-2-earn-ui/set-backend-url-prod.sh
@@ -0,0 +1,2 @@
+
+sed -i'' -e 's/http:\/\/localhost:8042/https:\/\/cultdonations.org/g' ./src/stores.js
---
+++ frontend/cult-learn-2-earn-ui/set-backend-url-prog.sh
@@ -0,0 +1,2 @@
+
+sed -i'' -e 's/https:\/\/cultdonations.org/http:\/\/localhost:8042/g' ./src/stores.js
