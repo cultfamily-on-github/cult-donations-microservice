4b118c0478ef406ab6d2632977e0bef175a23dfe
Michael <michael.spengler@sap.com>
2022-11-04 14:22:06 +0100

    leverage filecoin as ipfs storage incentive layer

---
+++ filecoin-lotus-miner-setup.sh
@@ -0,0 +1,30 @@
+# https://lotus.filecoin.io/lotus/install/linux/#start-the-lotus-daemon-and-sync-the-chain
+
+## system dependencies
+sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
+
+## rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## go
+wget -c https://golang.org/dl/go1.18.1.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+
+## path
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc && source ~/.bashrc
+
+## lotus
+git clone https://github.com/filecoin-project/lotus.git
+cd lotus
+git checkout releases # The releases branch contains the latest stable release
+
+## join mainnet
+make clean all
+
+## check
+lotus --version
+
+## start lotus
+lotus daemon
+
+## synch
+lotus sync wait
--- start-ipfs-service-on-boot.sh
+++ start-ipfs-service-on-boot.sh
@@ -3,3 +3,6 @@ systemctl start ipfs # to start the service
 systemctl enable ipfs # to make it run on boot
 journalctl -u ipfs.service # to check the logs - https://unix.stackexchange.com/questions/225401/how-to-see-full-log-from-systemctl-status-service
 
+# manual for cli 
+# ipfs add -r ./cult-uploads/
+# https://ipfs.io/ipfs/QmY2ZYnd5XEgnt6vtCCbHVQJt387tk4LVgA5xHAA8pLwnS
